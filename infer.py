a0de0279f28882fe62c1f8f5bb10cd0c6617e4f7
Mohamed Bakry Abd El-Gawad Mahmoud <154627225+MBOmran@users.noreply.github.com>
2026-04-19 23:28:28 +0200

    Update infer.py

--- infer.py
+++ infer.py
@@ -212,8 +212,8 @@ def main():
                 concated_result.paste(person_images[i], (0, 0))
                 concated_result.paste(cloth_images[i], (w, 0))  
                 concated_result.paste(result, (w*2, 0))
-                result = concated_result '''
-            result.save(output_path)
+                result = concated_result 
+            result.save(output_path)'''
         stem = Path(person_name).stem
         result.save(out_dir / f"{stem}_result.jpg", quality=95)
 
